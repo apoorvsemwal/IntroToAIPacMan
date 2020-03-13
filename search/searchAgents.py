c32d29f9fd9a4f48b36cdee4884f6df7e36b5175
Ayush <ayush.dave10@gmail.com>
2020-03-13 17:14:38 -0400

    Sub Optimal Path

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -533,7 +533,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.breadthFirstSearch(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -570,8 +570,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
